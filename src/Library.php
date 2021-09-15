bb31336cf2b4dbf85d8b4c24e3865c8e0ed894cc
邹景立 <zoujingli@qq.com>
2021-09-15 11:21:22 +0800

    修改注释代码

--- src/Library.php
+++ src/Library.php
@@ -83,11 +83,17 @@ class Library extends \think\Service
             $this->app->middleware->add(function (Request $request, \Closure $next) {
                 $header = [];
                 if (($origin = $request->header('origin', '*')) !== '*') {
-                    $header['Access-Control-Allow-Origin'] = $origin;
-                    $header['Access-Control-Allow-Methods'] = 'GET,PUT,POST,PATCH,DELETE';
-                    $header['Access-Control-Allow-Headers'] = 'Authorization,Content-Type,If-Match,If-Modified-Since,If-None-Match,If-Unmodified-Since,X-Requested-With,Api-Name,Api-Type,Api-Token,User-Form-Token,User-Token,Token';
-                    $header['Access-Control-Expose-Headers'] = 'Api-Name,Api-Type,Api-Token,User-Form-Token,User-Token,Token';
-                    $header['Access-Control-Allow-Credentials'] = 'true';
+                    $auto = $this->app->config->get('app.cors_auto', 1);
+                    $hosts = $this->app->config->get('app.cors_host', []);
+                    if (is_string($hosts)) $hosts = str2arr($hosts);
+                    if ($auto || in_array(parse_url(strtolower($origin), PHP_URL_HOST), $hosts)) {
+                        $headers = $this->app->config->get('app.cors_headers', 'Api-Name,Api-Type,Api-Token,User-Form-Token,User-Token,Token');
+                        $header['Access-Control-Allow-Origin'] = $origin;
+                        $header['Access-Control-Allow-Methods'] = $this->app->config->get('app.cors_methods', 'GET,PUT,POST,PATCH,DELETE');
+                        $header['Access-Control-Allow-Headers'] = "Authorization,Content-Type,If-Match,If-Modified-Since,If-None-Match,If-Unmodified-Since,X-Requested-With,{$headers}";
+                        $header['Access-Control-Expose-Headers'] = $headers;
+                        $header['Access-Control-Allow-Credentials'] = 'true';
+                    }
                 }
                 // 访问模式及访问权限检查
                 if ($request->isOptions()) {
